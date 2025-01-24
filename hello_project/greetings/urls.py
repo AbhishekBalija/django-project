6ac85dd59ccce309d164fb185bcab1b57406a63d
AbhishekBalija <abhiarsikere0001@gmail.com>
2025-01-24 21:00:41 +0530

    deleted old folders

--- hello_project/greetings/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path;
-from . import views;
-
-urlpatterns = [
-    path('', views.say_hello, name='say_hello'),
-    path('index/', views.index, name='index'),
-]
--- hello_project/greetings/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render;
-
-def say_hello(request):
-    return HttpResponse("Hello from Django!")
-
-def index(request):
-    return render(request, 'greeting/index.html')
